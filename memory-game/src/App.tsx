d6885ad82d4cefb50200419509e18b0fa44605f0
FolseV <folsefeeder@gmail.com>
2021-12-01 23:15:45 +0300

    feat: added Greeting component and link to it

--- memory-game/src/App.tsx
+++ memory-game/src/App.tsx
@@ -8,6 +8,7 @@ import react from "./img/cards/platforms/react.svg";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import { Navigate } from "react-router-dom";
+import Greeting from "./components/Greeting";
 
 function App() {
   const { cards } = useTypedSelector((state) => state.cards);
@@ -35,12 +36,15 @@ function App() {
         </header>
         <main>
           <Routes>
-            <Route path="/greetings" element={<h1>Hello</h1>} />
+            <Route path="/greetings" element={<Greeting />} />
             <Route path="/home" element={<Form />} />
             <Route path="/memorygame" element={<MemoryGame />} />
             <Route path="*" element={<Navigate to="/greetings" />}></Route>
           </Routes>
         </main>
+        <footer>
+          <div style={{ margin: "500px" }}>created by Folse</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
